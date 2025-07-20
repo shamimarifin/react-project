b7d40c8a063e02b5a0fc453fae0c6d79fa83c119
shamimarifin <shamimarifin790@gmail.com>
2025-07-20 16:56:52 +0600

    Hello World | React js app Code Flow

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from "./Header"
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <h1>Welcome to Bangladesh</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid ipsum, cumque libero, ut beatae unde ex sed, tempora suscipit ducimus cupiditate eum eos accusamus architecto quidem doloribus quia numquam.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Header(){
+    return(
+        <div>
+            <h1>My name is Shamim</h1>
+        </div>
+    )
+}
+
+export default Header;
