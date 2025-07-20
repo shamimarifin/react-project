15615d9471cabce772bd1025650d099139f2eeaa
shamimarifin <shamimarifin790@gmail.com>
2025-07-20 17:21:54 +0600

    Make first Component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,21 @@ function App() {
       <Header/>
       <h1>Welcome to Bangladesh</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid ipsum, cumque libero, ut beatae unde ex sed, tempora suscipit ducimus cupiditate eum eos accusamus architecto quidem doloribus quia numquam.</p>
+      <Fruit />
+      <Clock />
+    </div>
+  )
+}
+
+
+function Fruit(){
+  return <h1>Hello</h1>
+}
+
+function Clock(){
+  return(
+    <div>
+      <h2>What time is is?</h2>
     </div>
   )
 }
