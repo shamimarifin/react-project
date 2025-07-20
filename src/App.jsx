0660b6da58abe64998c03d6bf35cf168f0d804e7
shamimarifin <shamimarifin790@gmail.com>
2025-07-20 18:18:35 +0600

    Importing and Exporting Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-
-import Header from "./Header"
-
-function App() {
-
-  return (
-    <div>
-      <Header/>
-      <h1>Welcome to Bangladesh</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid ipsum, cumque libero, ut beatae unde ex sed, tempora suscipit ducimus cupiditate eum eos accusamus architecto quidem doloribus quia numquam.</p>
-      <Fruit />
-      <Clock />
-    </div>
-  )
-}
+import Login from "./userComponent";
 
 
-function Fruit(){
-  return <h1>Hello</h1>
-}
 
-function Clock(){
+
+function App(){
   return(
     <div>
-      <h2>What time is is?</h2>
+      <Login />
     </div>
   )
 }
 
-export default App
+
+export default App;
---
+++ src/UserComponent.jsx
@@ -0,0 +1,21 @@
+
+
+function Login(){
+    return(
+        <div>
+            <h1>User Component</h1>
+            <Profile />
+        </div>
+    )
+}
+
+
+function Profile(){
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Login
